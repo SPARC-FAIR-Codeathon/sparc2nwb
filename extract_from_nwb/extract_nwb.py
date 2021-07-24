d1be6abe447e55096f606432d837da3d10c72dbc
PatrickC <iwill0202@gmail.com>
2021-07-24 14:51:32 -0500

    creating dynamic function during class init

--- extract_from_nwb/extract_nwb.py
+++ extract_from_nwb/extract_nwb.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from pynwb import NWBHDF5IO
+from types import MethodType
 
 class EXTRACT_NWB():
     def __init__(self, file_path):
@@ -9,19 +10,32 @@ class EXTRACT_NWB():
         self.__nwbfile = nwbfile
         
         data_dict = {}
-#         data_dict['id'] = list(self.__nwbfile.electrodes.id)
+        data_dict_desc = {}
+        data_dict['id'] = list(nwbfile.electrodes.id.data)
         for col in nwbfile.electrodes.colnames:
             exec(f'data_dict["{col}"] = list(nwbfile.electrodes.{col}.data)')
+            exec(f'data_dict_desc["{col}"] = nwbfile.electrodes.{col}.description')
         #     print(nwbfile.electrodes.(col))
         self.__data_df = pd.DataFrame.from_dict(data_dict)
+        self.__data_dict_desc = data_dict_desc
         
-        defaultColLs = ['acquisition','general_device','subject','intervals','stimulus','units']
-        dataColLs = ['electrodes_description', 'electrodes', 'file_date', 'experiment_description', 'identifier']
+        defaultColLs = ['acquisition', 'devices', 'intervals', 'stimulus', 'units', 'subject']
+        dataColLs = ['electrodes_description', 'electrodes', 'experiment_description', 'identifier',
+                'session_description', 'session_start_time', 'lab', 'institution', 'epoch_tags',
+                'timestamps_reference_time','file_create_date', 'keywords', 'experimenter', 'related_publications']
         tmpAll = dataColLs + defaultColLs
-        
-        nwbColls = list(nwbfile.fields.keys())
-#         nwbColls
 
+        nwbColls = list(nwbfile.fields.keys())
+
+        str_fun = '''
+def get_%s(self):
+    print("it is in %s!")
+self.get_%s = MethodType(get_%s, self)'''
+
+        for i in nwbColls:
+            if i not in tmpAll: 
+                exec(str_fun%(i, i, i, i))
+        
     def get_nwb_source(self):
         return self.__nwbfile
     
@@ -29,8 +43,8 @@ class EXTRACT_NWB():
     def get_acquisition(self):
         return self.__nwbfile.acquisition
         
-    def get_general_device(self):
-        return self.__nwbfile.general
+    def get_devices(self):
+        return self.__nwbfile.devices
         
     def get_intervals(self):
         return self.__nwbfile.intervals
@@ -46,7 +60,7 @@ class EXTRACT_NWB():
         return self.__nwbfile.electrodes.description
     
     def get_electrodes(self):
-        return self.__data_df
+        return [self.__data_df, self.__data_dict_desc]
     
     def get_experiment_description(self):
         return self.__nwbfile.experiment_description
@@ -58,7 +72,7 @@ class EXTRACT_NWB():
         return dict(self.__nwbfile.fields['subject'].fields)   
     
     def get_session_description(self):
-    	return self.__nwbfile.session_description
+        return self.__nwbfile.session_description
     
     def get_session_start_time(self):
         return self.__nwbfile.session_start_time
@@ -70,7 +84,7 @@ class EXTRACT_NWB():
         return self.__nwbfile.file_create_date[0]
     
     def get_keywords(self):
-        return self.__nwbfile.keywords
+        return list(self.__nwbfile.keywords)
     
     def get_epoch_tags(self):
         return self.__nwbfile.epoch_tags
@@ -86,6 +100,4 @@ class EXTRACT_NWB():
     
     def get_related_publications(self):
         return self.__nwbfile.related_publications
-
-# testNWB = EXTRACT_NWB(fpath)
-# print('done')
+        
